aa8d99995ca06dc93ea681db1897ebed754785a1
TSequeiros <tamara@rojoconsultancy.com>
2017-04-25 16:16:56 +0200

    systemjs.config.server.js npm local

--- src/systemjs.config.server.js
+++ src/systemjs.config.server.js
@@ -6,7 +6,8 @@
 (function (global) {
     System.config({
         paths: {
-            'npm:': 'https://unpkg.com/' // path serves as alias
+            // 'npm:': 'https://unpkg.com/' // path serves as alias
+            'npm' : 'node_modules/'
         },
         // map tells the System loader where to look for things
         map: {
